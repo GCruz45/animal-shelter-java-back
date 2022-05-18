e53cf1c20039ff7ba4c8725fb1046230d8801f1b
Gabriel Cruz L <gabrielcl45@gmail.com>
2022-05-18 17:33:04 -0500

    Add contract tests

--- src/main/java/com/shelter/animalback/domain/Animal.java
+++ src/main/java/com/shelter/animalback/domain/Animal.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter @NoArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public class Animal {
     private String name;
     private String breed;
